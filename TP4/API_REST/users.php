b37ecbef85e489622af1b2dcb0c1a4b4612dd078
ben212001 <benjamin.puzenat@gmail.com>
2023-03-22 11:06:31 +0100

    v1 API_REST

---
+++ TP4/API_REST/users.php
@@ -0,0 +1,57 @@
+<?php
+    header("Content-Type: application/json; charset=UTF-8");
+
+    require_once('config.php');
+    require_once('init_pdo.php');
+
+    // on récupère la méthode la requête HTTP
+    $request_method = $_SERVER['REQUEST_METHOD'];
+    
+    // on récupère l'URL
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);  
+    
+    // on découpe l'URL selon les "/" et on range chaque partie dans un tableau
+    $uri = explode( '/', $uri );     
+    
+    //actions différentes en fonction du type de méthode HTTP utilisé
+    switch($request_method){
+        case 'GET':
+            $request = $pdo->prepare("SELECT * FROM Users");
+            $request->execute();
+            $resultat = $request->fetchAll(PDO::FETCH_OBJ);
+            $affich = json_encode($resultat);
+            echo $affich;
+            break;
+
+        case 'POST':
+            $json = json_decode(file_get_contents('php://input'));         
+            $name = $json['name'];
+            $email = $json['email'];         
+            $request = $pdo->prepare("INSERT INTO Users (name, email) VALUES ('$name', '$email')");         
+            $request->execute();         
+            $resultat = $request->fetchAll(PDO::FETCH_OBJ);
+            $affich = json_encode($resultat);
+            echo $affich;
+            break;
+
+        case 'PUT':
+            if(!empty($uri[5])) {
+                $json = json_decode(file_get_contents('php://input'), true);
+                $name = $json['name'];
+                $email = $json['email'];
+                $request = $pdo->prepare("UPDATE Users SET email='" . $email . "' WHERE id=" . $uri[5]);
+                $request->execute();
+                $request = $pdo->prepare("UPDATE Users SET name='" . $name . "' WHERE id=" . $uri[5]);
+                $request->execute();
+            }
+            break;
+
+        case 'DELETE':
+            if(!empty($uri[5])) {
+                $request = $pdo->prepare("DELETE FROM Users WHERE id=$uri[5])");
+                $request->execute();
+            }
+            break;
+    }
+
+    $pdo = null;
